2e5355d6e69c764fc41772ecafa0d0d558af3057
g_zawie <gzawie24@gmail.com>
2025-04-14 19:24:56 +0300

    Add ExpenseForm component and implement dynamic expense adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseTable from "./components/ExpenseTable";
-import expenses from "./data/expenses";
+import ExpenseForm from "./components/ExpenseForm";
+import initialExpenses from "./data/expenses";
 
 function App() {
+  const [expenses, setExpenses] = useState(initialExpenses);
+
+  function handleAddExpense(newExpense) {
+    setExpenses([...expenses, newExpense]);
+  }
+
   return (
     <div>
       <h1>Expense Tracker</h1>
+      <ExpenseForm onAddExpense={handleAddExpense} />
       <ExpenseTable expenses={expenses} />
     </div>
   );
